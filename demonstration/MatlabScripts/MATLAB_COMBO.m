3988cdf4f4f21fc45f5f1cc833a357c5a0baae41
Gerry Politis <G.Politis@warwick.ac.uk>
2015-04-24 15:36:03 +0100

    Improved the browsing

--- demonstration/MatlabScripts/MATLAB_COMBO.m
+++ demonstration/MatlabScripts/MATLAB_COMBO.m
@@ -3,15 +3,19 @@ clc
 close all
 
 % Insert the name of the file e.g. smallnetwork_adj   
-filename = input('Insert the name of the file without the file type (e.g. example) : ','s'); % Insert the txt file's name
+%filename = input('Insert the name of the file without the file type (e.g. example) : ','s'); % Insert the txt file's name
 
+uiwait(msgbox('Browse to your file'));
+
+[FileName,PathName] = uigetfile('*.txt','Select the input file of .txt format');
+[filename tmp] = strread(FileName, '%s %s', 'delimiter','.');
+filename = char(filename); 
 
 % IN ADJACENCY MATRIX FORMAT
-cd ..
+
 tmppwd = pwd;
-file = fullfile(cd, ['\InputData\' sprintf(filename) '.txt']);
+file = fullfile([PathName sprintf(FileName)]);
 data = load(file);
-cd ([pwd '\MatlabScripts'])
 
 % Transform it to andat edje list
 edje_list = adj2edgeL(data);
@@ -38,7 +42,7 @@ edje_list = adj2edgeL(data);
 % to be handled by the COMBO algorithm
 
 edgeL2pajek(edje_list, sprintf([filename '.net']));
-movefile(sprintf([filename '.net']),[tmppwd '\ComboCode'])
+movefile(sprintf([filename '.net']),[fileparts(pwd) '\ComboCode'])
 
 %% Running the C++ executable
 flag = 'run';
@@ -51,10 +55,10 @@ while flag == 'run'
     modularity = runCpp(filename,communities);
     
     % Insert the output of the Combo algorithm
-    cd ..
-    file = fullfile(cd, ['\ComboCode\' sprintf(filename) '_comm_comboC++.txt']);
+    
+    file = fullfile([fileparts(pwd) '\ComboCode\' sprintf(filename) '_comm_comboC++.txt']);
     comm = load(file);
-    cd ([pwd '\MatlabScripts'])
+    
     
     disp(' ')
     disp(['The Combo algorithm has detected ' num2str(length(unique(comm))) ' communities'] )
@@ -85,7 +89,7 @@ while flag == 'run'
     
 end
 
-break
+
 %% Pair the communities found to the nodes
 
 new_node_list = zeros(length(comm),3);
@@ -96,24 +100,23 @@ end
 
 %% Output to xls file with headers
 
-cd ..
-mkdir('Output_for_Gephi')
-cd ([pwd '\Output_for_Gephi'])
-if exist([filename '.csv']) ~= 0
+
+mkdir( [fileparts(pwd)], '\Output_for_Gephi')
+
+if exist([ fileparts(pwd) '\Output_for_Gephi' filename '.csv']) ~= 0
     %     temp_n = exist([filename '.csv']);
     %     filename_temp = [filename num2str(temp_n)];
     % else
     %     filename_temp = [filename];
     
-    delete([filename '.csv']);
+    delete([fileparts(pwd) '\Output_for_Gephi' filename '.csv']);
 end
 
 col_header = {'ID','Label','Node_type'};
-xlswrite([filename '.csv'],new_node_list,'Sheet1','A2');     %Write data
-xlswrite([filename '.csv'],col_header,'Sheet1','A1');     %Write column header
+xlswrite([[fileparts(pwd) '\Output_for_Gephi\'] filename '.csv'],new_node_list,'Sheet1','A2');     %Write data
+xlswrite([[fileparts(pwd) '\Output_for_Gephi\'] filename '.csv'],col_header,'Sheet1','A1');     %Write column header
 
-cd ..
-cd ([pwd '\MatlabScripts'])
+
 
 
 %% Unscramble the adjacency matrix containing the original data
@@ -134,9 +137,19 @@ cd ([pwd '\MatlabScripts'])
 % sorted_data = sorted_data(:,k)
 
 % Obtaing the correct order (unshuffling) using the Sparse reverse Cuthill-McKee ordering 
+
+
 order = symrcm(data);
-sorted_data = K(k,:);
-sorted_data = sorted_data(:,k);
+sorted_data = data(order,:);
+sorted_data = sorted_data(:,order);
+
+mkdir( [fileparts(pwd)], '\Sorted_Adj_Output')
+if exist([ fileparts(pwd) '\Sorted_Adj_Output' filename '_sorted_matrix.txt']) ~= 0   
+    delete([fileparts(pwd) '\Sorted_Adj_Output' filename '_sorted_matrix.txt']);
+end
+
+dlmwrite([fileparts(pwd) '\Sorted_Adj_Output' filename '_sorted_matrix.txt'],sorted_data)
+
 
 
 
